ab678bf409bb9d9fd873789dad507923541d2a6a
RizkiB12 <rbagus306@gmail.com>
2024-04-12 13:23:32 +0700

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Bagus',
+            'email' => 'bagus@example.com',
+            'password' => bcrypt('123.321A'),
+            'email_verified_at' => time()
+        ]);
+
+        //create fake project and the tasks
+        Project::factory()
+        ->count(30)
+        ->hasTasks(30)
+        ->create();
+    }
+}
